bb614971a20eb865ab8a97ed446352be37006f24
Primology <istevkovski@outlook.com>
2018-07-17 23:53:09 +0200

    Improvements

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ import sidenav from './components/sidenav'
 import sharedMethods from './sharedMethods.js'
 import Contact from './components/Contact'
 
+
 export default {
     name: 'App',
     data() {
@@ -34,6 +35,19 @@ export default {
 
     },
     mounted: function() {
+        const vueThis = this;
+        // vueThis.$store.commit('setNavActiveTracker', 1);
+        $(function() {
+            $.scrollify({
+                section: ".section",
+                setHeights: false,
+                scrollSpeed: 650,
+                updateHash: false,
+                before: function(index, sections) {
+                    vueThis.$store.commit('setNavActiveTracker', (index + 1));
+                }
+            });
+        });
 
     },
     mixins: [sharedMethods]
@@ -58,7 +72,7 @@ export default {
         box-shadow: 0px 6px 7px rgba(0, 0, 0, 0.15);
     }
 
-    @media only screen and (max-width: 1366px) {
+    @media only screen and (max-width: $responsive-xl) {
         #app {
             margin: 0;
         }
--- src/sharedMethods.js
+++ src/sharedMethods.js
@@ -7,6 +7,13 @@ export default {
         },
         setNavActiveTracker(data) {
             this.$store.commit('setNavActiveTracker', data)
+        },
+        slideDetectScroll: function() {
+            $(document).on('scroll', function() {
+                if($(this).scrollTop()>=$('#section-about').position().top){
+                    this.$store.commit('setNavActiveTracker', 2);
+                }
+            });
         }
     }
 }
